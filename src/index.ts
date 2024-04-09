9d3254ae997fc5878bd20899aba86cf994963816
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-04-09 22:16:24 +0800

    Implement compiler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
+import { $ } from "bun";
 import { Hono } from "hono";
+
+interface Body {
+  language: string;
+  code: string;
+  input: string;
+}
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.post("/", async (c) => {
+  const { language, code, input } = await c.req.json<Body>();
+  const hash = Bun.hash(code).toString(16).substring(0, 8);
+
+  // Write the source code to a file
+  const sourceFile = `${hash}.${language}`;
+  await Bun.write(sourceFile, code);
+
+  // Compile the source code
+  const binaryFile = `${hash}`;
+  const { stderr, exitCode } = await $`gcc ${sourceFile} -o ${binaryFile}`
+    .nothrow()
+    .quiet();
+  await $`rm ${sourceFile}`;
+  if (exitCode !== 0) {
+    return c.json({ stderr: stderr.toString() });
+  }
+
+  // Run the compiled binary
+  const command =
+    input === "" ? $`./${binaryFile}` : $`./${binaryFile} < ${input}`;
+  const { stdout } = await command.nothrow().quiet();
+  await $`rm ${binaryFile}`;
+  return c.json({ stdout: stdout.toString() });
 });
 
 export default app;
